98e7ba2342dab54ea6ae1d5c20552eaa3714dc01
Felipe Gené <felipegenef@gmail.com>
2022-11-27 14:33:09 -0300

    functioning

--- LambdaDocker/entrypoint.sh
+++ LambdaDocker/entrypoint.sh
@@ -5,13 +5,13 @@ if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   mkdir -p ../mnt/efs/data
   touch ../mnt/efs/data/schema.prisma
   cp -f ./prisma/schema.prisma ../mnt/efs/data/schema.prisma
-  /usr/local/bin/npx prisma migrate dev --name migration --schema="../mnt/efs/data/schema.prisma"
+  # /usr/local/bin/npx prisma migrate dev --name migration --schema="../mnt/efs/data/schema.prisma"
   /usr/local/bin/aws-lambda-rie /usr/bin/npx aws-lambda-ric $1
 else
   mkdir -p ../mnt/efs/data
   touch ../mnt/efs/data/schema.prisma
   cp -f ./prisma/schema.prisma ../mnt/efs/data/schema.prisma
-  /usr/local/bin/npx prisma migrate dev --name migration --schema="../mnt/efs/data/schema.prisma"
+  # /usr/local/bin/npx prisma migrate dev --name migration --schema="../mnt/efs/data/schema.prisma"
   /usr/local/bin/npx aws-lambda-ric $1
 fi  
 
--- LambdaDocker/server.js
+++ LambdaDocker/server.js
@@ -1,28 +1,54 @@
-const { PrismaClient } = require("@prisma/client");
+const { Sequelize, UUID, UUIDV4 } = require("sequelize");
+const { resolve } = require("path");
 
-let prisma;
+let connection;
 /**
  * Returns All models from application
- * @returns {PrismaClient}
+ * @returns {{ connection, User:import("sequelize") }}
  */
 async function buildCache() {
-  if (!global.prisma) {
-    console.log("**INFO", "Building new Connection.");
-    global.prisma = await new PrismaClient();
-    prisma = global.prisma;
-    return prisma;
+  if (!global.connection) {
+    const sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: resolve("../", "mnt", "efs", "data", "database.db"),
+    });
+    global.connection = sequelize;
+    const connection = global.connection;
+    const User = connection.define("users", {
+      id: {
+        type: UUID,
+        primaryKey: true,
+        defaultValue: UUIDV4,
+      },
+    });
+    await sequelize.authenticate();
+
+    await sequelize.sync({ alter: true });
+    console.log("**INFO", "Creating new connection");
+    return { connection, User };
   }
+  /**
+   * @type {Sequelize}
+   */
+  const connection = global.connection;
+  const User = connection.define("users", {
+    id: {
+      type: UUID,
+      primaryKey: true,
+      defaultValue: UUIDV4,
+    },
+  });
   console.log("**INFO", "Using existing Connection");
-  prisma = global.prisma;
-  return prisma;
+  await connection.sync({ alter: true });
+
+  return { connection, User };
 }
+
 module.exports.handler = async (event) => {
   try {
-    const connection = await buildCache();
-    const response = await connection.user.create({
-      data: { email: new Date(), name: "name" },
-    });
-    const data = await connection.user.findMany();
+    const { User } = await buildCache();
+    await User.create({});
+    const data = await User.findAll();
     console.log(data);
     return {
       statusCode: 200,

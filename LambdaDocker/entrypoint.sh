06bd33d444b6c2825ffb01d3b0b0c87c6e39ed7b
Felipe Gené <felipegenef@gmail.com>
2022-12-02 00:39:40 -0300

    prisma

--- LambdaDocker/entrypoint.sh
+++ LambdaDocker/entrypoint.sh
@@ -6,14 +6,17 @@ mkdir -p ../mnt/efs/logs
 mkdir -p ../mnt/efs/prisma
 cp -r ./prisma/schema.prisma ../mnt/efs/prisma/schema.prisma
 npx prisma generate --schema ../mnt/efs/prisma/schema.prisma
-npx prisma migrate dev --name teste  --schema ../mnt/efs/prisma/schema.prisma
+npx prisma migrate dev --name migration --schema ../mnt/efs/prisma/schema.prisma
 NODE_ENV=local node server.js
 else
 # AWS
-  mkdir -p ../mnt/efs/data
-  mkdir -p ../mnt/efs/logs
-  # /usr/local/bin/npx sequelize-cli db:migrate
-  /usr/local/bin/npx aws-lambda-ric $1
+mkdir -p ../mnt/efs/data
+mkdir -p ../mnt/efs/logs
+mkdir -p ../mnt/efs/prisma
+cp -r ./prisma/schema.prisma ../mnt/efs/prisma/schema.prisma
+/usr/local/bin/npx prisma generate --schema ../mnt/efs/prisma/schema.prisma
+/usr/local/bin/npx prisma migrate dev --name migration --schema ../mnt/efs/prisma/schema.prisma
+/usr/local/bin/npx aws-lambda-ric $1
 fi  
 
 
--- LambdaDocker/server.ts
+++ LambdaDocker/server.ts
@@ -1,21 +1,24 @@
 import * as serverless from "serverless-http";
-import { Router } from "express";
-import express from "express";
+import * as express from "express";
 import userRouter from "./routes/user";
 import docsRouter from "./routes/docs";
 import * as swaggerUi from "swagger-ui-express";
 
 import { PrismaClient } from "@prisma/client";
-
+const Router = express.Router;
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 const baseRoute = Router();
 app.use("/course", baseRoute);
-baseRoute.get("/create", (req, res) => {
-  prisma.user.create({ data: { email: "email", name: "nome" } });
+baseRoute.get("/create", async (req, res) => {
+  await prisma.user.create({ data: { email: "email", name: "nome" } });
   res.send("salvo");
+});
+baseRoute.get("/create", async (req, res) => {
+  const data = await prisma.user.findMany();
+  res.send({ data });
 });
 baseRoute.use("/user", userRouter);
 baseRoute.use("/docs", docsRouter);
